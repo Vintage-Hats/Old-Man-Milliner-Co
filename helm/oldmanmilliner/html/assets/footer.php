792ea825f26b9b14443dbed9368ee4ee9fcc9edb
Unomisan <yunomie@gmail.com>
2018-06-09 20:53:36 -0400

    Testing upload of multiple files [web]

---
+++ helm/oldmanmilliner/html/assets/footer.php
@@ -0,0 +1,15 @@
+<?php
+echo "			<p class='temp'>Footer.php code</p>\n\n";
+
+?>
+			<div class='nav' id='bottom'>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="shop.php">Emporium</a></li>
+					<li><a href="about/php">About Us</a></li>
+					<li><a href="tips.php">Hat Tricks</a></li>
+				</ul>
+				<p id='disclaim'>We own none of this except the code.</p>
+			</div>
+		</body>
+	</html>
