26f95f74ce169691edef13a71500ed15e9fe7020
Unomisan <yunomie@gmail.com>
2018-06-23 22:48:38 -0400

    Final pieces for the BIG TEST

--- helm/oldmanmilliner/html/index.php
+++ helm/oldmanmilliner/html/index.php
@@ -1,25 +1,31 @@
+
 <?php
 	require_once 'assets/functions.php';
 	require_once 'assets/header.php';
-	require_once 'assets/search.php';
-	require_once 'assets/promo.php';
+	require_once 'assets/leftCol.php';
+	require_once 'assets/rightColTop.php';
+?>
 
-	echo "			<div id='main'>";
-			
-		$sql = 'SELECT * FROM hats;';
-		$result = $conn->query($sql);
-		echo "\n				<ul>\n";
-		while ($hat = $result->fetch_array()) {
-			echo "					<li><strong>".$hat['hatName']."</strong><br>".
-										"<strong>Style:</strong> ".$hat['style']." ".
-										"<strong>from</strong> ".$hat['label']."<br>".
-										"<strong>Crown height:</strong> ".$hat['crown']." ".
-										"<strong>Brim width:</strong> ".$hat['brim']."<br>".
-										"<strong>Description:</strong><br>".$hat['description']."</li><br>\n";
-		}
-		echo "				</ul>\n";
-		
-	echo "			</div>\n";
-			
+					<div id='content'> <!-- Main content window. Place page specific content inside this div tag. -->
+					<h2>You Do You!</h2> 
+						<p>There's much more to great style than following the trends. Knowing the history of style and fashion helps the
+							individual to express their uniqueness. Old Man Milliner understands.</p>
+						<p>This site presents not only great hats from the past, but also the knowledge to wear them well. Search our store
+							by selecting attributes that matter to the only person that really counts here: you. We carry a wide selection
+							of hats to help find the just the right statement to top everything off.</p>
+						<h2>Emporium</h2>
+						<p>Find just the right hat at just the right price. WIth pics of each of the hats, filter and sort options, the
+							Emporium enables you narrow your search to just those attributes you want.</p>
+						<h2>Hat Tricks</h2>
+						<p>We want our customers to not only like their hats, but love their look in their hats. We've dedicated pages to
+							helping you find the right size, color, and style. We want your experience with the hats we sell to be long and
+							enjoyable. Your hat says much about you and the pages of tips, tricks, and knowledge make finding the right
+							statement easier.</p>
+						<h2>About Us</h2>
+						<p>Want to know more about Old Man Milliner &amp; Co.? Find out where we are, how to contact us, and our history.</p>
+					</div> <!-- End of content window. Do not replace anything afte this. -->
+					<img src='images/h-banner1.png' class='hBanner'>
+
+<?php
 	require_once 'assets/footer.php';
 ?>
---
+++ helm/oldmanmilliner/html/shop.php
@@ -0,0 +1,10 @@
+
+<?php
+	require_once 'assets/functions.php'; // contains: 
+	require_once 'assets/header.php';
+	require_once 'assets/leftCol.php'; 
+	require_once 'assets/rightColTop.php';
+	include_once 'assets/storeBuilder.php';
+	echo "					<img src='images/h-banner1.png' class='hBanner'>\n";
+	require_once 'assets/footer.php';
+?>
