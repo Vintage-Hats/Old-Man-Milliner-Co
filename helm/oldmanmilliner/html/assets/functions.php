4cbe1c517c6d29b26787c46e466f6364c52bdac2
Unomisan <yunomie@gmail.com>
2018-06-25 21:25:18 -0400

    bandaid for db access issue

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,40 +1,21 @@
 <?php
-	require_once '/var/www/html/cred.php';
+	require_once 'cred.php';
 
-/*[t] CONNECTION TOGGLE :: FIXME: remove for production*/
-	// LOCALHOST 
-	function q($query) {
-		$c = getC("1");
-		$conn = new mysqli($c[0], $c[1], $c[2], $c[3]);
-		unset($c);
-		$records = $conn->query($query);
-		return $records;
-	}
+function q($query) {
 
-	/*/
-
-	// OMM
-	function q($query) {
-		$c = getC("0"); //FIXME: change to no longer send an argument
+/*  // *
+	$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
+	$p = 3306;
+	$s = "";
+	$u = "milliner";
+	$w = "ilikehats";
+	$n = "oldmanmilliner";
+// */
+		$c = fetchC();
 		$conn = new mysqli($c['h'], $c['u'], $c['w'], $c['n'], $c['p'], $c['s'])
 			or die ('Could not connect to the database server' . mysqli_connect_error());
 		unset($c);
 		$records = $conn->query($query);
 		return $records;
 	}
-
-// */ // END CONNECTION TOGGLE
-
-/* TEST: DESCRIBE RECORDS toggle *
-
-	$rows = $records->num_rows;
-	// parse and display data
-	for ($i = 0; $i < $rows; ++$i ) {
-		$records->data_seek($i);
-		$record = $records->fetch_array(MYSQLI_ASSOC);
-		echo $record['id'].": ".$record['hatName']."<br>";
-	}
-
-// */ // END DESCRIBE RECORDS
-
 ?>
