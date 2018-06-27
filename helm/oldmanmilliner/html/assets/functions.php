5b078a337c7da4dff3b3f19873c1c83a7d234c29
Unomisan <yunomie@gmail.com>
2018-06-26 23:48:27 -0400

    Added switch to change where to look for cred.php

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,16 +1,20 @@
 <?php
-	require_once 'cred.php';
 
-function q($query) {
+	/* set credFlipper equal...
+		... to 1 to use cred located in the assets folder
+		... to 2 to use cred located in the html folder
+		... to 3 to use cred located in the www folder
+	*/
 
-/*  // *
-	$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
-	$p = 3306;
-	$s = "";
-	$u = "milliner";
-	$w = "ilikehats";
-	$n = "oldmanmilliner";
-// */
+	$credFlipper = 1;
+	switch ($credFlipper) {
+		case 1: require_once 'cred.php'; 	break;
+		case 2: require_once '../cred.php';	break;
+		case 3: require_once '../../cred.php';	break;
+	}
+
+
+	function q($query) {
 		$c = fetchC();
 		$conn = new mysqli($c['h'], $c['u'], $c['w'], $c['n'], $c['p'], $c['s'])
 			or die ('Could not connect to the database server' . mysqli_connect_error());
