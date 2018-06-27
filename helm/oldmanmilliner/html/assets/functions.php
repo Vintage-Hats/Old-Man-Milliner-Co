3af2c60c9a83d4379e6572f96fc837a803918dee
Unomisan <yunomie@gmail.com>
2018-06-27 00:18:50 -0400

    Correction to fourth-option-only experiment

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,12 +1,11 @@
-<?php		
-	$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
-	$p = 3306;
-	$s = "";
-	$u = "milliner";
-	$w = "ilikehats";
-	$n = "oldmanmilliner";
-
-	function q($query) {
+<?php
+	function q($query) {		
+		$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
+		$p = 3306;
+		$s = "";
+		$u = "milliner";
+		$w = "ilikehats";
+		$n = "oldmanmilliner";
 		$conn = new mysqli($h, $u, $w, $n, $p, $s)
 			or die ('Could not connect to the database server' . mysqli_connect_error());
 		$records = $conn->query($query);
