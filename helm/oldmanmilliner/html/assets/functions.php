3acfe7e1c851b50e2418337eded77ed07fcd2915
Unomisan <yunomie@gmail.com>
2018-06-28 20:34:26 -0400

    Removed test

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,17 +1,13 @@
 <?php
-	if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-	    echo 'We don\'t have mysqli!!!';
-	} else {
-		function q($query) {		
-			$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
-			$p = 3306;
-			$s = "";
-			$u = "milliner";
-			$w = "ilikehats";
-			$n = "oldmanmilliner";
-			$conn = new mysqli($h, $u, $w, $n, $p, $s);
-			$records = $conn->query($query);
-			return $records;
-		}
+	function q($query) {		
+		$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
+		$p = 3306;
+		$s = "";
+		$u = "milliner";
+		$w = "ilikehats";
+		$n = "oldmanmilliner";
+		$conn = new mysqli($h, $u, $w, $n, $p, $s);
+		$records = $conn->query($query);
+		return $records;
 	}
 ?>
