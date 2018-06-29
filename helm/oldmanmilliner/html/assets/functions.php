47675fceed7fe856787ba186c8360aa90176df4f
Unomisan <yunomie@gmail.com>
2018-06-28 20:32:18 -0400

    deploy install test for mysql

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,13 +1,17 @@
 <?php
-	function q($query) {		
-		$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
-		$p = 3306;
-		$s = "";
-		$u = "milliner";
-		$w = "ilikehats";
-		$n = "oldmanmilliner";
-		$conn = new mysqli($h, $u, $w, $n, $p, $s);
-		$records = $conn->query($query);
-		return $records;
+	if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+	    echo 'We don\'t have mysqli!!!';
+	} else {
+		function q($query) {		
+			$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
+			$p = 3306;
+			$s = "";
+			$u = "milliner";
+			$w = "ilikehats";
+			$n = "oldmanmilliner";
+			$conn = new mysqli($h, $u, $w, $n, $p, $s);
+			$records = $conn->query($query);
+			return $records;
+		}
 	}
 ?>
