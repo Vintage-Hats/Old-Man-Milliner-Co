36795d332e79b3b1ad88d116605077392333ca15
Unomisan <yunomie@gmail.com>
2018-07-24 16:52:03 -0400

    updating connection logic

--- helm/oldmanmilliner/html/assets/functions.php
+++ helm/oldmanmilliner/html/assets/functions.php
@@ -1,12 +1,11 @@
 <?php
-	function q($query) {		
-		$h = "milliner.cbtlze2bnbgy.us-east-1.rds.amazonaws.com";
-		$p = 3306;
-		$s = "";
-		$u = "milliner";
-		$w = "ilikehats";
-		$n = "oldmanmilliner";
-		$conn = new mysqli($h, $u, $w, $n, $p, $s);
+	require_once 'cred.php';
+
+	function q($query) {
+		$c = fetchC();
+		$conn = new mysqli($c['h'], $c['u'], $c['w'], $c['n'], $c['p'], $c['s'])
+			or die ('Could not connect to the database server' . mysqli_connect_error());
+		unset($c);
 		$records = $conn->query($query);
 		return $records;
 	}
